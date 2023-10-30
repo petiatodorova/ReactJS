fc22c2af9841f6de6ae8fd044dcf3fcfb3db732a
Petya Todorova <invisioncl@gmail.com>
2023-10-30 21:11:58 +0200

    JSX Demo

---
+++ ViteDemo/src/App.jsx
@@ -0,0 +1,26 @@
+import About from "./components/About"
+import Clients from "./components/Clients"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Services from "./components/Services"
+import Team from "./components/Team"
+import Why from "./components/Why"
+import Info from "./components/Info"
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Services />
+      <About />
+      <Why />
+      <Team />
+      <Clients />
+      <Info />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
