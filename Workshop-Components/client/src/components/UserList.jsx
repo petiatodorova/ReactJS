950b38757063e52f2ba5c8847aaa12746e02eae7
Petya Todorova <invisioncl@gmail.com>
2023-11-05 19:24:43 +0200

    Added folder components.

---
+++ Workshop-Components/client/src/components/UserList.jsx
@@ -0,0 +1,15 @@
+import Search from "./Search";
+import UserListTable from "./UserListTable";
+
+const UserList = () => {
+    return (
+        <section className="card users-container">
+            <Search />
+
+            <UserListTable />
+        </section>
+
+    );
+}
+
+export default UserList;
